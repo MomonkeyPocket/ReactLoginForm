0c2f98e5c222b3f7e07a9f5863cefb49fe15d765
azkatang <mog2to4aki@gmail.com>
2022-10-13 14:51:28 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formContainer">
+      <form>
+        <h1>ログインフォーム</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>ユーザー名</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="ユーザー名"
+              // value={formValues.username}
+              // onChange={(e) => handleChange(e)}
+            />
+          </div>
+          {/* <p className="errorMsg">{formErros.username}</p> */}
+          <div className="formField">
+            <label>メールアドレス</label>
+            <input
+              type="text"
+              name="mailAddress"
+              placeholder="メールアドレス"
+              // value={formValues.mailAddress}
+              // onChange={(e) => handleChange(e)}
+            />
+          </div>
+          {/* <p className="errorMsg">{formErros.mailAddress}</p> */}
+
+          <div className="formField">
+            <label>パスワード</label>
+            <input
+              type="text"
+              name="password"
+              placeholder="パスワード"
+              // value={formValues.password}
+              // onChange={(e) => handleChange(e)}
+            />
+          </div>
+          {/* <p className="errorMsg">{formErros.password}</p> */}
+          <button className="submitButton">登録</button>
+          {/* {Object.keys(formErros).length === 0 && isSubmit && (
+            <div className="msgOk">ログインに成功しました</div>
+          )} */}
+        </div>
+      </form>
     </div>
   );
 }
